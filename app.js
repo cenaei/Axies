2bea948940864646c5c3ed080bbc61d8c6a20666
Sina Sangiji <sina.sangiji@gmail.com>
2022-07-07 17:33:39 +0430

    Hero: Animation Added

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const splashScreen = document.querySelector(".splash-screen")
-const searchEl = document.getElementsByClassName("header__search")
 
 window.addEventListener('DOMContentLoaded', (event) => {
   setTimeout(() => {
     splashScreen.classList.add("hidden")
   }, 2500)
 });
-
-// searchEl.addEventListener('click', (event) => {
-
-// })
