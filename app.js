338cf6583142d3996cd80c0d24f9a955f5d8afd7
Sina Sangiji <sina.sangiji@gmail.com>
2022-07-07 17:14:36 +0430

    Search: Added Input

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const splashScreen = document.querySelector(".splash-screen")
+const searchEl = document.getElementsByClassName("header__search")
 
 window.addEventListener('DOMContentLoaded', (event) => {
   setTimeout(() => {
     splashScreen.classList.add("hidden")
   }, 2500)
 });
+
+searchEl.addEventListener('click', (event) => {
+  
+})
